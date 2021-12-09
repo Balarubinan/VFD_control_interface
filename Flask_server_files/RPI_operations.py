0035845ac066c73b9aaece87c559dacd38c56ed2
Balarubinan <sudhagouthirubi@gmail.com>
2021-12-09 07:57:00 +0000

    * added addressing and ADC input

--- Flask_server_files/RPI_operations.py
+++ Flask_server_files/RPI_operations.py
@@ -1,5 +1,19 @@
+import smbus
+import time
 def get_current_value():
     pass
 
 def calculate_alttitude_values():
     pass
+
+def measure_voltage():
+    addr=0x48
+    A0=0x40
+    bus = smbus.SMBus(1)
+    while True:
+        bus.write_byte(addr, A0)
+        value = bus.read_byte(addr)
+        print(value)
+        time.sleep(0.1)
+
+measure_voltage()
